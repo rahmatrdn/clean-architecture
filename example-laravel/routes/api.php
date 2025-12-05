b26144fe9f10da75c67443dd2230cc6bc5500c20
Rahmat Ramadhan Putra <rahmat.jinggolabs@gmail.com>
2025-12-05 22:48:29 +0700

    Add task category API routes
    Introduces a new route group for task categories, providing endpoints for listing, creating, retrieving, updating, and deleting task categories via TaskCategoryController.

--- example-laravel/routes/api.php
+++ example-laravel/routes/api.php
@@ -14,3 +14,11 @@ Route::prefix('task')->group(function () {
     Route::put('/{task}', [\App\Http\Controllers\Api\TaskController::class, 'update']);
     Route::delete('/{task}', [\App\Http\Controllers\Api\TaskController::class, 'destroy']);
 });
+
+Route::prefix('task-category')->group(function () {
+    Route::get('/', [\App\Http\Controllers\Api\TaskCategoryController::class, 'index']);
+    Route::post('/', [\App\Http\Controllers\Api\TaskCategoryController::class, 'store']);
+    Route::get('/{category}', [\App\Http\Controllers\Api\TaskCategoryController::class, 'show']);
+    Route::put('/{category}', [\App\Http\Controllers\Api\TaskCategoryController::class, 'update']);
+    Route::delete('/{category}', [\App\Http\Controllers\Api\TaskCategoryController::class, 'destroy']);
+});
